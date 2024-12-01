8b73dd4f1e4e89c0a1bc81b852a5ac82e5c647da
Sohann22 <np01cp4a220376@islingtoncollege.edu.np>
2024-12-01 14:54:06 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+require('./database/connection')
+
+const UserRoute = require('./routes/userRoutes')
+
+const app = express()
+
+const port = process.env.PORT
+
+
+app.use(express.json())
+
+app.use(UserRoute)
+
+app.listen(port,  ()=>console.log("APP STARTED SUCCESSFULLY"))
